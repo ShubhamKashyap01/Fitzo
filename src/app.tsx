6390f5dacc68f099eb4fd856ef057c6852dee606
ShubhamKashyap01 <shubhamk191@gmail.com>
2023-05-02 23:21:27 +0530

    slot page added

--- src/app.tsx
+++ src/app.tsx
@@ -3,14 +3,9 @@ import logo from "./logo.svg";
 import "./app.css";
  import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Home from "./pages/Home/Home";
+import Slots from './components/Slots/Slots'
 
 export function App() {
-
-  // useEffect(() => {
-  //   fetch("/api/v1")
-  //     .then((data) => data.json())
-  //     .then((data) => setApiInfo(data));
-  // }, []);
 
   return (
     <>
@@ -20,7 +15,10 @@ export function App() {
         <Route path="login" element={<h2>Login</h2>}/>
         <Route path="signup" element={<h2>signup</h2>}/>
         <Route path="*" element={<h2>defualt</h2>}/>
-        <Route path="activity" element={<h2>activity</h2>}/>
+        <Route path=":location" >
+          <Route path="" element={<Home/>}/>
+          <Route path=":activity" element={<Slots/>}/>
+        </Route>
       </Routes>
     </Router>
     </>
